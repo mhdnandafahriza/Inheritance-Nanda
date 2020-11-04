3d562fa602a6b9613f4817fe429b695b304cd47b
mhdnandafahriza <61672373+mhdnandafahriza@users.noreply.github.com>
2020-11-04 16:07:20 +0700

    Add files via upload

---
+++ MyApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApplication
+{
+  class Program
+  { 
+    static void Main(string[] args)
+    {
+      
+      Car myCar = new Car();
+
+      
+      myCar.honk();
+
+     
+      Console.WriteLine(myCar.brand + " " + myCar.modelName);
+    }
+  }
+}
